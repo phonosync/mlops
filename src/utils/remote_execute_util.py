f5657e02e2cc221877557ccd57c6ccfbe1849786
Manuel Dömer <Manuel_Doemer@swissre.com>
2019-08-30 13:53:21 +0200

    ws authentication in case of mulitple tenants
    added local_predict

--- src/utils/remote_execute_util.py
+++ src/utils/remote_execute_util.py
@@ -1,15 +1,28 @@
 
 from azureml.core import Workspace, Experiment
 from azureml.train.estimator import Estimator
+from azureml.core.authentication import InteractiveLoginAuthentication
 
 from utils.aml_util import prepare_remote_compute
 import os
 
 
-def prepare_remote_experiment(regularization):
+def get_ws(tenant=None):
+    """
+    :param tenant:
+    :return:
+    """
+    if tenant:
+        auth = InteractiveLoginAuthentication(tenant_id = tenant)
+        ws = Workspace.from_config(auth = auth)
+    else:
+        ws = Workspace.from_config()
+    return ws
+
+def prepare_remote_experiment(ws, regularization):
     # Connect to remote Machine Learning Services Workspace (MLSW).
     # Needs config.json in the root of the project. You can download it from portal.azure.com -> MLSW
-    ws = Workspace.from_config()
+
     ds = ws.get_default_datastore()
 
     #Check that remote computing exists
